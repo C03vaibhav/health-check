74b72ff91847181ed035ce81e72ce576b41b3a9a
Vaibhav CTR <c03vaibhav@gmail.com>
2023-10-01 18:44:13 +0530

    Chnages made in print statements

--- 2.py
+++ 2.py
@@ -14,19 +14,19 @@ def is_prime(num):
     return True
 
 # Initialize variables for sum, prime count, and composite count
-sum_of_numbers = 0
-prime_count = 0
-composite_count = 0
+num_sum = 0
+prime_num_count = 0
+composite_num_count = 0
 
 # Loop through numbers from 1 to 100
 for num in range(1, 101):
-    sum_of_numbers += num
+    num_sum += num
     if is_prime(num):
-        prime_count += 1
+        prime_num_count += 1
     else:
-        composite_count += 1
+        composite_num_count += 1
 
 # Print the results
-print("Sum of numbers from 1 to 100:", sum_of_numbers)
-print("Number of prime numbers in the range:", prime_count)
-print("Number of composite numbers in the range:", composite_count)
+print("Sum of numbers from 1 to 100:", num_sum)
+print("Number of prime numbers in the range:", prime_num_count)
+print("Number of composite numbers in the range:", composite_num_count)
