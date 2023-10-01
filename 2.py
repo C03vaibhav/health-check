42cd2c3179d97b0a2980f3012441076583cfc55d
Vaibhav CTR <c03vaibhav@gmail.com>
2023-10-01 18:51:22 +0530

    Variables names added

--- 2.py
+++ 2.py
@@ -15,19 +15,19 @@ def is_prime(num):
 
 # Initialize variables for sum, prime count, and composite count
 
-num_sum = 0
-prime_num_count = 0
-composite_num_count = 0
+nums_sum = 0
+primes_num_count = 0
+composites_num_count = 0
 
 # Loop through numbers from 1 to 100
 for num in range(1, 101):
-    num_sum += num
+    nums_sum += num
     if is_prime(num):
-        prime_num_count += 1
+        primes_num_count += 1
     else:
-        composite_num_count += 1
+        composites_num_count += 1
 
 # Print the results
-print("Sum of numbers from 1 to 100:", num_sum)
-print("Number of prime numbers in the range:", prime_num_count)
-print("Number of composite numbers in the range:", composite_num_count)
+print("Sum of numbers from 1 to 100:", nums_sum)
+print("Number of prime numbers in the range:", primes_num_count)
+print("Number of composite numbers in the range:", composites_num_count)
