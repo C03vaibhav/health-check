527566bb1411f3292b5ac11c707f5399e79b5d5b
Vaibhav CTR <c03vaibhav@gmail.com>
2023-10-02 11:22:28 +0530

    6.py square formual sum added

--- 6.py
+++ 6.py
@@ -7,3 +7,6 @@ n=100
 formula_sum=n*(n+1)/2
 print('Summ of first 100 natural numbers using formula is:',formula_sum)
     
+n=100
+formula_sq_sum=n*(n+1)*(2*n+1)/6
+print('Summ of square of first 100 natural numbers using formula is:',formula_sq_sum)
